c7840c9ea0111b2d3872301ef2c8e9e6e96172b0
BRC-Thomas <thomasbaracani@gmail.com>
2023-10-03 22:03:59 +0200

    Intersection Observer & Search & Errors Msg

--- src/hooks/usePhotos.js
+++ src/hooks/usePhotos.js
@@ -1,26 +1,46 @@
 import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 
-export default function usePhotos({ querySearch, pageIndex }) {
+export default function usePhotos(querySearch, pageIndex) {
   const [error, setError] = useState({
     msg: "",
     state: false,
   });
-
   const [photos, setPhotos] = useState([]);
   const [maxPages, setMaxPages] = useState(0);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if (photos.length !== 0 && maxPages !== 0) {
+      setPhotos([]);
+      setMaxPages(0);
+    }
+  }, [querySearch]);
+
+  useEffect(() => {
+    setLoading(true);
+
     fetch(
       `https://api.unsplash.com/search/photos?page=${pageIndex}&per_page=30&query=${querySearch}&client_id=${
         import.meta.env.VITE_KEY
       }`
     )
-      .then((res) => res.json())
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(`${response.status} Error, something went wrong`);
+
+        return response.json();
+      })
       .then((data) => {
-        setPhotos(data.results);
+        setPhotos((state) => [...state, ...data.results]);
         setMaxPages(data.total_pages);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError({
+          msg: err.message,
+          state: true,
+        });
         setLoading(false);
       });
   }, [querySearch, pageIndex]);
