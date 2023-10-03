9168431bf2fb38720b02a7d2b9afd02dd7f0d4a2
BRC-Thomas <thomasbaracani@gmail.com>
2023-10-03 18:06:01 +0200

    Hook perso + fetch api

---
+++ src/components/List.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, useRef } from "react";
+import spinner from "../assets/spinner.svg";
+import usePhotos from "../hooks/usePhotos";
+
+export default function List() {
+  const [query, setQuery] = useState("random");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const photosApiData = usePhotos(query, pageNumber);
+  console.log(photosApiData);
+
+  return (
+    <>
+      <h1 className="text-4xl">Unsplash Infinite Scroll</h1>
+      <form>
+        <label htmlFor="search" className="block mb-4">
+          Look for images...
+        </label>
+        <input
+          type="text"
+          placeholder="Look for something..."
+          className="block w-full mb-14 text-slate-800 py-3 px-2 text-md outline-gray-500 rounded border border-slate-400"
+        />
+      </form>
+      <ul className="grid grid-cols-[repeat(auto-fill,minmax(250px,_1fr))] auto-rows-[175px] gap-4 justify-center">
+        {!photosApiData.loader &&
+          photosApiData.photos.length !== 0 &&
+          photosApiData.photos.map((photo, index) => (
+            <li key={photo.id}>
+              <img
+                className="w-full h-full object-cover"
+                src={photo.urls.regular}
+                alt={photo.alt_description}
+              />
+            </li>
+          ))}
+      </ul>
+
+      {/* Loader */}
+      {photosApiData.loading && !photosApiData.error.state && (
+        <img src={spinner} alt="loader" className="block mx-auto" />
+      )}
+    </>
+  );
+}
